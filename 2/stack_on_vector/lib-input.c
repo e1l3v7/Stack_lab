1deab2ffc17b45cde5a2d1e93f57de4221813a52
_._._._ <90711724+e1l3v7@users.noreply.github.com>
2024-05-12 15:40:49 +0300

    Update lib-input.c

--- 2/stack_on_vector/lib-input.c
+++ 2/stack_on_vector/lib-input.c
@@ -1,6 +1,21 @@
-#ifndef LIB-INPUT_H
-#define LIB-INPUT_H
+#include <stdio.h>
 
-void Scanf(int* a);
-
-#endif
+int Scanf(int* a)
+{
+	int flag=scanf("%d",a);
+	while(flag != 1 && flag != EOF)
+	{
+		printf("введен символ, а не число, повторите ввод\n");
+		scanf("%*[^\n]");
+		flag=scanf("%d",a);
+	}
+	if(flag == 1)
+	{
+		return 1;
+	}
+	if(flag == EOF)
+	{
+		printf("\nEOF!!!\n");
+		return 0;
+	}
+}
