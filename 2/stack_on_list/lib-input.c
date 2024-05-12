731014cf6bad9de7acfc9adcf96158118e86636e
_._._._ <90711724+e1l3v7@users.noreply.github.com>
2024-05-12 15:40:00 +0300

    Update lib-input.c

--- 2/stack_on_list/lib-input.c
+++ 2/stack_on_list/lib-input.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
-#include "lib-input.h"
-void Scanf(int* a)
+
+int Scanf(int* a)
 {
-	while(scanf("%d",a) != 1)
+	int flag=scanf("%d",a);
+	while(flag != 1 && flag != EOF)
 	{
 		printf("введен символ, а не число, повторите ввод\n");
 		scanf("%*[^\n]");
+		flag=scanf("%d",a);
+	}
+	if(flag == 1)
+	{
+		return 1;
+	}
+	if(flag == EOF)
+	{
+		printf("\nEOF!!!\n");
+		return 0;
 	}
 }
